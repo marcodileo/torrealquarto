208110926179fc1ca0d7407392864c9420a49017
marcodileo <marco@architettodileo.com>
2017-09-26 21:28:52 +0200

    Update grayscale.js

--- js/grayscale.js
+++ js/grayscale.js
@@ -187,7 +187,7 @@ function init() {
     map: map,
     icon: image
   });
-  google.maps.event.addListener(marker, 'click', function () {
-  window.location.href = 'http://www.google.com';
-});
+  google.maps.event.addListener(beachMarker, 'click', function() {
+        window.open('https://www.youtube.com/watch?v='+this.url);
+    });
 }
