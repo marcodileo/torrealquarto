cd9f28434ddf6c28ec2e84a3cb6fabe8d05baa2d
marcodileo <marco@architettodileo.com>
2017-09-26 21:07:03 +0200

    Update grayscale.js

--- js/grayscale.js
+++ js/grayscale.js
@@ -50,7 +50,7 @@ function init() {
   // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
   var mapOptions = {
     // How zoomed in you want the map to start at (always required)
-    zoom: 15,
+    zoom: 12,
 
     // The latitude and longitude to center the map (always required)
     center: new google.maps.LatLng(41.294832, 15.930493), // New York
